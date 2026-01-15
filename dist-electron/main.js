9e244b15f8e27b1829eb673e70e36cd771731b90
fabiopmfilho <fabiopmfilho@outlook.com>
2026-01-15 04:06:01 -0300

    fix: remove scroll bar

--- dist-electron/main.js
+++ dist-electron/main.js
@@ -13,10 +13,12 @@ let win;
 function createWindow() {
   win = new BrowserWindow({
     icon: path.join(process.env.VITE_PUBLIC, "electron-vite.svg"),
+    // autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname$1, "preload.mjs")
     }
   });
+  win.setMenu(null);
   win.webContents.on("did-finish-load", () => {
     win == null ? void 0 : win.webContents.send("main-process-message", (/* @__PURE__ */ new Date()).toLocaleString());
   });
